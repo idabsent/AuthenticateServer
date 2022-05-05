8e196c71b82fa5afbcb90bacde9879f3942104f1
idabsent <idabsentgi@gmail.com>
2022-05-04 17:11:14 -0700

    manifest file created, docker file added and port changed

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.20"
     kotlin("plugin.serialization") version "1.6.20"
+    application
 }
 
 group = "org.example"
@@ -28,10 +29,30 @@ dependencies {
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
 }
 
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to application.mainClass.get()
+        )
+    }
+
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map { if (it.isDirectory()) { it } else { zipTree(it) } }
+
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("org.example.main.AuthServerKt")
 }
